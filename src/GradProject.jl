cb43550a33725877c68daa8096d1bfcca6442594
ndgnuh <ndgnuh@protonmail.com>
2021-05-28 17:17:24 +0700

    Try catch loop

--- src/GradProject.jl
+++ src/GradProject.jl
@@ -2,9 +2,14 @@ module GradProject
 
 Model = include("Model.jl")
 Dashboard = include("Dashboard.jl")
+Replication = include("Replication.jl")
 
 if !isempty(PROGRAM_FILE) && realpath(PROGRAM_FILE) == @__FILE__
-	Dashboard.start()
+    try
+        Dashboard.start()
+    catch e
+        showerror(stdout, e, catch_backtrace())
+    end
 end
 
 end # module
