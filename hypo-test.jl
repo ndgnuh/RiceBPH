3c93b93c115a82dcaa0ab4f3e8c5e8b58e0a317d
ndgnuh <ndgnuh@protonmail.com>
2021-06-10 00:51:11 +0700

    Auto hypothesis test script

---
+++ hypo-test.jl
@@ -0,0 +1,21 @@
+using Distributed
+@everywhere using Pkg
+@everywhere Pkg.activate(@__DIR__)
+
+@everywhere begin
+    using GradProject
+    using GradProject.PostProcess
+    using ProgressMeter
+    using CSV, DataFrames
+    const testdir = "kiem-dinh"
+    const resultdir = "results"
+end
+
+mkpath(testdir)
+
+let cases = Iterators.product([0.01, 0.001, 0.0], [0.1, 0.05, 0.005])
+    @showprogress pmap(cases) do (p0, a)
+        df = PostProcess.batch_test_rice(resultdir, p0; alpha=a, clean=true)
+        CSV.write(joinpath(testdir, "test-p0+$p0-alpha+$a.csv"), df)
+    end
+end
