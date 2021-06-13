747dcc9f306d4af0da5af07c8997ce3c763f79d3
ndgnuh <ndgnuh@protonmail.com>
2021-06-14 03:30:52 +0700

    [post process] Peak population

--- src/PostProcess.jl
+++ src/PostProcess.jl
@@ -4,6 +4,58 @@ using DataFrames
 using JLD2
 using PlotlyJS
 using HypothesisTests
+using Clustering
+
+function ma(X, k)
+    pad = zeros(eltype(X), k ÷ 2)
+    return [pad; [mean(X[i:(i + k)]) for i in 1:(length(X) - k)]; pad]
+end
+
+function peak_population(X::AbstractVector; smooth=48 * 7 ÷ 2, threshold=0.0)
+    # Smooth signal
+    Y = X
+    Y = ma(X, smooth)
+    # Normlize
+    MX = maximum(Y)
+    mX = minimum(Y)
+    Y = (Y .- mean(Y)) ./ std(Y)
+    # Find the peaks
+    return peaks = let r = Y .≥ threshold
+        ranges = findall(isone, abs.(diff(r)))
+        if isodd(length(ranges))
+            push!(range, length(X))
+        end
+        map(Iterators.partition(ranges, 2)) do (a, b)
+            _, offset = findmax(@view X[a:b])
+            a + offset
+        end
+    end
+end
+
+function peak_population(file::AbstractString; kwargs...)
+    peaks = jldopen(file) do f
+        map(1:1000) do seed
+            df = f["$seed"]
+            peak_population(df.count_bph; kwargs...)
+        end
+    end
+    peaks = let lens = length.(peaks)
+        s = std(lens)
+        m = mean(lens)
+        a, b = m - 3s, m + 3s
+        filter(x -> a ≤ length(x) ≤ b, peaks)
+    end
+    npeaks = maximum(length.(peaks))
+    peaks = reduce(vcat, peaks)
+
+    cl = kmeans(transpose(peaks), npeaks)
+    peaks = map(1:npeaks) do k
+        peaks[k .== cl.assignments]
+    end
+    mpeaks = mean.(peaks)
+    perm = sortperm(mpeaks)
+    return peaks[perm]
+end
 
 function bname(fpath)
     return split(fpath, r"[\\/]")[end]
@@ -75,7 +127,7 @@ function is_flower_effective(t; alpha=0.05)
     # Ha: flower is effective -> p < p0 -> tail = left
     # good -> reject H0 -> alpha > pvalue
     #a, b = confint(t; tail=:right, level=1-alpha)
-    return alpha ≥ pvalue(t, tail=:left)
+    return alpha ≥ pvalue(t; tail=:left)
 end
 
 function test_rice(files::AbstractVector, p0; alpha=0.05)
@@ -103,13 +155,30 @@ function batch_test_rice(dir::AbstractString, p0; alpha, clean=false)
     end
     df = test_rice(files, p0; alpha=alpha)
     if clean
+        transform!(df, :test => ByRow(is_flower_effective) => :h0)
         select(df, Not("test"))
     else
         df
     end
 end
 
-export JLD2, PlotlyJS, ProgressMeter, HypothesisTest, plot, savefig, test_rice
+function batch_peak_population(dir::AbstractString; kwargs...)
+    files = joinpath.(dir, readdir(dir))
+    map(files) do file
+        file => peak_population(file; kwargs...)
+    end
+end
+
+export JLD2,
+    PlotlyJS,
+    ProgressMeter,
+    HypothesisTest,
+    plot,
+    savefig,
+    test_rice,
+    peak_population,
+    batch_test_rice,
+    batch_peak_population
 
 # end module
 end
