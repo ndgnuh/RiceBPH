034a0409be0e86535159f423c34e3ca17c81de7e
ndgnuh <ndgnuh@protonmail.com>
2021-06-14 19:21:48 +0700

    Add test for peak population

--- test/postprocesstest.jl
+++ test/postprocesstest.jl
@@ -1,6 +1,7 @@
 using RiceBPH.PostProcess
+using JLD2
 
-result_dir = joinpath("..", "results")
+result_dir = joinpath(@__DIR__, "sample-results")
 result_files = joinpath.(result_dir, readdir(result_dir))
 result_files = filter(endswith("jld2"), result_files)
 result_file = rand(result_files)
@@ -17,7 +18,16 @@ end
 @testset "Hypothesis Testing" begin
     test_rice(result_file, 0.01)
     @test true
-    df = test_rice(result_files, 0.01)
-    display(df)
+    batch_test_rice(result_dir, 0.01; alpha=0.05, clean=true)
+    @test true
+end
+
+@testset "Population peak" begin
+    local df = jldopen(f -> f["1"], result_file)
+    peak_population(df.count_bph)
+    @test true
+    peak_population(result_file)
+    @test true
+    batch_peak_population(result_dir)
     @test true
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using Test
 using BenchmarkTools
 using RiceBPH
 
-#include("simutest.jl")
+include("simutest.jl")
 
 include("postprocesstest.jl")
