6537ceb99ca4371aff7d6fab57afb7bbb7b121a6
Nguyễn Đức Hùng <ndgnuh@protonmail.com>
2022-06-26 15:24:52 +0700

    Fix name + add deps

---
+++ src/RiceBPH.jl
@@ -0,0 +1,8 @@
+module RiceBPH
+
+using Base: @kwdef
+using Agents
+
+greet() = print("Hello World!")
+
+end # module RiceBPH
--- src/RiceBPH2.jl
+++
@@ -1,5 +0,0 @@
-module RiceBPH2
-
-greet() = print("Hello World!")
-
-end # module RiceBPH2
