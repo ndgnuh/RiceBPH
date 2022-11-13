6805581cb4552ab5055f21e1462bcfdd6d9ced7f
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-11-14 00:11:40 +0700

    benchmark script

---
+++ scripts/run_benchmark.jl
@@ -0,0 +1,16 @@
+const BASEPATH = joinpath(@__DIR__, "..")
+using Pkg
+Pkg.activate(BASEPATH)
+
+using RiceBPH
+using BenchmarkTools
+
+const MAPSPATH = joinpath(BASEPATH, "assets", "envmaps")
+const MAP = joinpath(MAPSPATH, rand(readdir(MAPSPATH)))
+
+bm = @benchmark RiceBPH.run_simulation(; seed=0,
+                                       init_nb_bph=200,
+                                       init_pr_eliminate=0.05,
+                                       init_position=:corner,
+                                       envmap=MAP)
+display(bm)
