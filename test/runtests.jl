d47d178b542659b67d0547ba6134f6149ceb2699
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-11-21 09:21:54 +0700

    benchmark

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,26 +1,12 @@
 using Test
 #= using BenchmarkTools =#
 using RiceBPH: init_model, AGENT_DATA, MODEL_DATA, get_plot_kwargs
-using GLMakie
-using InteractiveDynamics
+using RiceBPH
+using BenchmarkTools
+using InteractiveUtils
 
-model, agent_step!, model_step! = let
-    model_kwargs = (seed=0,
-                    init_nb_bph=200,
-                    init_pr_eliminate=0.15,
-                    init_position=:corner,
-                    envmap="../assets/envmaps/015-1x2.csv")
-    init_model(; model_kwargs...)
-end
-
-fig, obs = abmexploration(model;
-                          (agent_step!)=agent_step!,
-                          (model_step!)=model_step!,
-                          adata=AGENT_DATA,
-                          mdata=MODEL_DATA,
-                          get_plot_kwargs(model)...)
-scene = display(fig)
-wait(scene)
-
-#= include("simutest.jl") =#
-#= include("postprocesstest.jl") =#
+@code_warntype RiceBPH.run_simulation(; seed=0,
+                                      init_nb_bph=200,
+                                      init_pr_eliminate=0.15,
+                                      init_position=:corner,
+                                      envmap="../assets/envmaps/015-1x2.csv")
