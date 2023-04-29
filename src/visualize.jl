6d53a0ee27e624c6eaf2cad4cd5cc4669a7b062c
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-04-29 18:32:30 +0700

    RE: visualisation

--- src/visualize.jl
+++ src/visualize.jl
@@ -3,87 +3,52 @@
 # they are loaded lazily using Requires, so that if there are
 # other tasks such as collecting statistics, they won't be loaded
 # and takes tons of time.
-module Visualize
+module Visualisations
 
-using Agents
-using Requires
+using Colors
+using GLMakie
+using ..Models
 
-function __init__()
-    @require GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a" @eval using GLMakie
-    @require Colors = "5ae59095-9a9b-59fe-a467-6f913c188581" @eval using Colors
-    @require InteractiveDynamics = "ec714cd0-5f51-11eb-0b6e-452e7367ff84" @eval using InteractiveDynamics
-end
+const FLOWER_COLOR = colorant"#FFC107"
+const RICE_COLORS = RGBAf.(0.0f0, 0.8f0, 0.0f0, 0.0f0:0.001f0:1.0f0)
+const AGENT_COLORS = Dict(Models.Egg => colorant"#0D47A1",
+                          Models.Nymph => colorant"#D32F2F",
+                          Models.Adult => colorant"#F44336")
 
-# Agent coloring
-struct AgentColor <: Function
-    model::AgentBasedModel
-end
+ac(agent) = AGENT_COLORS[agent.stage]
+am(_) = :circle
 
-function (ac::AgentColor)(agent)
-    x, y = agent.pos
-    model = ac.model
-    if isnan(model.food[x, y])
-        RGBAf(0.0f0, 0.0f0, 0.0f0)
-    elseif agent.age < model.age_init
-        RGBAf(0.0f0, 0.0f0, 1.0f0)
-    else
-        RGBAf(1.0f0, 0.0f0, 0.0f0)
-    end
-end
-
-# Agent marker
-function agent_marker(agent)
-    return agent.isshortwing ? :circle : :utriangle
-end
-
-# Model map
-function model_heatarray(model)
-    return model.food
+function heatarray(model)
+    flower_mask = model.flower_mask
+    @. !flower_mask * NaN32 + model.rice_map * flower_mask
 end
 
 function video(videopath::String,
-    crop,
-    init_nb_bph,
-    position,
-    pr_eliminate0;
-    seed,
-    frames=2880,
-    kwargs...)
+               crop,
+               init_nb_bph,
+               position,
+               pr_eliminate0;
+               seed,
+               frames = 2880,
+               kwargs...)
     @info "Video seed: $seed"
-    model = init_model(crop, init_nb_bph, position, pr_eliminate0; seed=seed, kwargs...)
+    model = init_model(crop, init_nb_bph, position, pr_eliminate0; seed = seed, kwargs...)
     return abm_video(videopath,
-        model,
-        agent_step!,
-        model_step!;#
-        frames=frames,
-        framerate=24,
-        ac=ac(model),
-        am=am,
-        heatarray=heatarray,
-        heatkwargs=(nan_color=(1.0, 1.0, 0.0, 0.5),
-            colormap=[(0, 1.0, 0, i) for i in 0:0.01:1],
-            colorrange=(0, 1)))
+                     model,
+                     agent_step!,
+                     model_step!;#
+                     frames = frames,
+                     framerate = 24,
+                     ac = ac(model),
+                     am = am,
+                     heatarray = heatarray,
+                     heatkwargs = (nan_color = (1.0, 1.0, 0.0, 0.5),
+                                   colormap = [(0, 1.0, 0, i) for i in 0:0.01:1],
+                                   colorrange = (0, 1)))
 end
 
-function default_map_color()
-    (;
-        nan_color=RGBA(1.0, 1.0, 0.0, 0.5),
-        colormap=[RGBA(0, 1.0, 0, i) for i in 0:0.01:1],
-        colorrange=(0, 1)
-    )
-end
-
-function default_agent_marker()
-    Dict(true => :circle, false => :utriangle)
-end
-
-function get_plot_kwargs(model)
-    return (frames=2880,
-        framerate=24,
-        ac=AgentColor(model),
-        am=agent_marker,
-        heatarray=model_heatarray,
-        heatkwargs=default_map_color())
-end
+const heatkwargs = (; nan_color = FLOWER_COLOR,
+                    colormap = RICE_COLORS,
+                    colorrange = (0, 1))
 
 end
