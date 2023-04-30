b9a40ed90545247aa5fa439a75bd024ae308099f
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-04-30 17:14:04 +0700

    use JDF to serialize instead of CSV

--- scripts/run_ofaat.jl
+++ scripts/run_ofaat.jl
@@ -1,20 +1,12 @@
 using Distributed
 using Comonicon
-using CSV
+using JDF
 using RiceBPH.OFAAT: run_ofaat!
 using TOML
 
-#= const num_steps = 2880 * 2 =#
-#= const num_replicates = 300 =#
-#= const map_size = 250 =#
-#= const flower_width = 0 =#
-#= const init_pr_eliminate = 0.0f0 =#
-#= const num_init_bphs = 200 =#
-#= const factor = :energy_transfer =#
-#= const values = collect(range(start = 2.5f-2, stop = 2.5f-1, length = 10)) |> reverse =#
+const OUTPUT_DIRECTORY = "outputs"
 
-# Bootstrap run
-@main function main(config_file::String; output::String = "output.csv")
+@main function main(config_file::String; output::String)
     config = TOML.parsefile(config_file)
     options = Dict(Symbol(k) => v for (k, v) in config)
     @info options
@@ -26,5 +18,7 @@ using TOML
     num_steps = pop!(options, :num_steps)
     num_replicates = pop!(options, :num_replicates)
     result = run_ofaat!(num_steps, num_replicates, factor, values; options...)
-    CSV.write(output, result)
+    output_directory = joinpath(OUTPUT_DIRECTORY, output)
+    JDF.save(output_directory, result)
+    @info "Output written to $(output_directory)"
 end
