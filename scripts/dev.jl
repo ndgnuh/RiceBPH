ba227a11b79b6c8d7fb48f47bb5e8f660a4c3d5f
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-01 03:47:53 +0700

    remove unused scripts

--- scripts/dev.jl
+++ scripts/dev.jl
@@ -1,32 +1,8 @@
-using RiceBPH.Models
-using RiceBPH.Visualisations
-using InteractiveDynamics
-using BenchmarkTools
-using GLMakie
+using CSV
+using DataFrames
+using RiceBPH.MyProtoBuf
 
-const MDATA = Models.MDATA_EXPL
-const model = init_model(; seed = 0,
-                         map_size = 100, num_init_bphs = 200,
-                         init_pr_eliminate = 0.05f0, flower_width = 0,
-                         init_position = Models.Corner,
-                         energy_transfer = 0.045f0)
-const _, mdf = run!(model, agent_step!, model_step!, 1; mdata = MDATA)
-#= @btime run!(model, agent_step!, model_step!, 2880) =#
+const df = CSV.read("./ofaat-nums-bph-init.csv", DataFrame)
 
-#= const heatkwargs = (nan_color = RGBf(1.0f0, 1.0f0, 0.0f0), =#
-#=                     colormap = [RGBf(0.0f0, i, 0.0f0) for i in 0.0f0:0.001f0:1.0f0], =#
-#=                     colorrange = (0, 1)) =#
-
-const heatkwargs = (;
-                    nan_color = RGBAf(1.0, 1.0, 0.0, 0.5),
-                    colormap = [RGBAf(0, 0.7, 0, i) for i in 0:0.01:1],
-                    colorrange = (0, 1))
-
-const fig, _ = abmexploration(model;
-                              (agent_step!) = agent_step!,
-                              (model_step!) = model_step!,
-                              mdata = MDATA,
-                              Visualisations.ac, Visualisations.heatarray,
-                              Visualisations.heatkwargs)
-const scene = display(fig)
-wait(scene)
+const pb = MyProtoBuf.from_dataframe(df)
+@show pb
