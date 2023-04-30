f5901c4da08512e2d4b07e7f6a64fa034afddf64
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-01 03:29:19 +0700

    remove redundant stuffs

--- src/ofaat.jl
+++ src/ofaat.jl
@@ -2,21 +2,17 @@ module OFAAT
 
 using ProgressMeter
 using Distributed
-using CSV
 using DataFrames
 using Printf: Format, format
 using ..Models: init_model, agent_step!, model_step!, run!, MDATA
 
-function generate_name(seed::Integer, factor::Symbol, value)
-    fmt = Format("%s-%.4f+seed-%04d.csv")
-    name = format(fmt, factor, value, seed)
+function generate_name(seed::Integer, factor::Symbol, value; suffix = ".jdf")
+    fmt = Format("%s-%.4f+seed-%04d.%s")
+    name = format(fmt, factor, value, seed, suffix)
     return joinpath(tempdir(), name)
 end
 
 function run_replicate!(num_steps, seed, factor, value, kw)
-    # Generate a name
-    output_csv = generate_name(seed, factor, value)
-
     # Run the simulation
     model = init_model(; factor => value, kw...)
     _, mdf = run!(model, agent_step!, model_step!, num_steps;
@@ -38,20 +34,12 @@ function run_ofaat!(num_steps::Integer,
                     values;
                     kw...)
     @info kw
-    list_files = String[]
     result = mapreduce(vcat, values) do value
         @info "Running $(factor) = $(value)"
         map_func(seed) = run_replicate!(num_steps, seed, factor, value, kw)
         reduce(vcat, @showprogress pmap(map_func, 1:num_replicates))
     end
 
-    # Merge result CSVs
-    #= @info "Merging results" =#
-    #= result = mapreduce(vcat, list_files) do output_file =#
-    #=     mdf = CSV.read(output_file, DataFrame) =#
-    #=     rm(output_file) =#
-    #=     return mdf =#
-    #= end =#
     return result
 end
 
