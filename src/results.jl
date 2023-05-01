36b5633f2598fdbba5b610aed1e2a3f35ab4385d
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-01 21:52:41 +0700

    use 3eps to make the pct equals when number is zero

--- src/results.jl
+++ src/results.jl
@@ -10,13 +10,13 @@ using Chain
 function infer_stats!(df)
     df.num_bphs = @. Float32(df.num_nymphs + df.num_brachys + df.num_macros)
     df.pct_nymphs = @. Float32((df.num_nymphs + eps(Float32)) /
-                               (df.num_bphs + eps(Float32)))
+                               (df.num_bphs + 3 * eps(Float32)))
     df.pct_macros = @. Float32((df.num_macros + eps(Float32)) /
-                               (df.num_bphs + eps(Float32)))
+                               (df.num_bphs + 3 * eps(Float32)))
     df.pct_brachys = @. Float32((df.num_brachys + eps(Float32)) /
-                                (df.num_bphs + eps(Float32)))
+                                (df.num_bphs + 3 * eps(Float32)))
     df.pct_females = @. Float32((df.num_females + eps(Float32)) /
-                                (df.num_bphs + eps(Float32)))
+                                (df.num_bphs + 2 * eps(Float32)))
     df.pct_males = @. Float32(1 - df.pct_females)
     return df
 end
