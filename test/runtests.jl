fa1fe1191aafd835cc4bd477bd038588c054e7a0
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-02 17:08:50 +0700

    miss-configured test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,18 @@
 using RiceBPH.Models
+using ProgressMeter
 using Test
 
-function test_replication(seed)
-    energy_transfer = 0.04f0
-    flower_width = 4
-    init_pr_eliminate = 0.05f0
+const NUM_STEPS = 2880
+
+function test_replication(seed, flower_width, init_pr_eliminate)
+    energy_transfer = 0.032f0
     map_size = 125
     num_init_bphs = 200
     model1 = init_model(; seed, num_init_bphs, energy_transfer, map_size, flower_width, init_pr_eliminate)
-    _, mdf1 = run!(model1, agent_step!, model_step!, 200, mdata=Models.MDATA)
+    _, mdf1 = run!(model1, agent_step!, model_step!, NUM_STEPS, mdata=Models.MDATA)
 
     model2 = init_model(; seed, num_init_bphs, energy_transfer, map_size, flower_width, init_pr_eliminate)
-    _, mdf2 = run!(model2, agent_step!, model_step!, 200, mdata=Models.MDATA)
+    _, mdf2 = run!(model2, agent_step!, model_step!, NUM_STEPS, mdata=Models.MDATA)
 
 
     return mdf1 == mdf2
@@ -19,7 +20,10 @@ end
 
 
 @testset "Replication test" begin
-    for seed in rand(1:1000, 5)
-        @test test_replication(1)
+    @showprogress for seed in 1:20
+        @test test_replication(seed, 4, 0.05f0)
+    end
+    @showprogress for seed in 1:20
+        @test test_replication(seed, 0, 0.0f0)
     end
 end
