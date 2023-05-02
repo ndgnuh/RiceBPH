a4718a4260116a17406e262a7726d253c8f8186d
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-02 16:21:12 +0700

    missing Package compiler

--- scripts/build_system_image.jl
+++ scripts/build_system_image.jl
@@ -2,13 +2,13 @@ using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 Pkg.resolve()
 
-#= let orig_load_path = joinpath(first(Base.DEPOT_PATH) , "packages") =#
-#=     push!(Base.LOAD_PATH, orig_load_path) =#
-#= end =#
 using PackageCompiler
 const packages = collect(keys(Pkg.installed()))
 const sysimage_path = "ricebph.sys.so"
 
-@show packages
-create_sysimage(packages; sysimage_build_args=`-O0`, sysimage_path=sysimage_path)
-@info sysimage_path
+const package_str = mapreduce(join, packages) do pkg
+    "\n\t- $pkg"
+end
+@info "Building system images with these packages:\n$(package_str)"
+create_sysimage(packages; sysimage_build_args = `-O0`, sysimage_path = sysimage_path)
+@info "System image written to $(sysimage_path)"
