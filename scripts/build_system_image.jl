c1fed2e8370eb88045a0093470e90da65a5b10b7
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-02 16:25:18 +0700

    build system image

--- scripts/build_system_image.jl
+++ scripts/build_system_image.jl
@@ -6,9 +6,7 @@ using PackageCompiler
 const packages = collect(keys(Pkg.installed()))
 const sysimage_path = "ricebph.sys.so"
 
-const package_str = mapreduce(join, packages) do pkg
-    "\n\t- $pkg"
-end
-@info "Building system images with these packages:\n$(package_str)"
+const package_str = join(packages, ", ")
+@info "Building system images with these packages:\n- $(package_str)"
 create_sysimage(packages; sysimage_build_args = `-O0`, sysimage_path = sysimage_path)
 @info "System image written to $(sysimage_path)"
