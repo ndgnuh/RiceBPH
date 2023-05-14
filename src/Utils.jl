26e474bff11732dfbdb90fe7f6e4897264b0c590
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-14 12:29:16 +0700

    refactor

--- src/Utils.jl
+++ src/Utils.jl
@@ -31,6 +31,20 @@ macro return_if(expr)
     end
 end
 
-export @easy_name_map, @return_if
+latex_name(name::Symbol) = latex_name(Val(name))
+latex_name(name::String) = latex_name(Symbol(name))
+latex_name(::Val{:num_init_bphs}) = "N_I"
+latex_name(::Val{:num_bphs}) = "n_A"
+latex_name(::Val{:num_nymphs}) = "n_N"
+latex_name(::Val{:num_eggs}) = "n_E"
+latex_name(::Val{:num_brachys}) = "n_B"
+latex_name(::Val{:num_macros}) = "n_M"
+latex_name(::Val{:pct_rices}) = "r_R"
+latex_name(::Val{:pct_nymphs}) = "r_N"
+latex_name(::Val{:pct_brachys}) = "r_B"
+latex_name(::Val{:pct_macros}) = "r_M"
+latex_name(::Val{:energy_transfer}) = "E_T"
+
+export @easy_name_map, @return_if, latex_name
 
 end
--- src/Visualisations.jl
+++ src/Visualisations.jl
@@ -1,23 +1,10 @@
 module Visualisations
 
 using DataFrames
+using .Utils: latex_name
 
 include("visualisations/agents.jl")
 include("visualisations/recipes.jl")
 include("visualisations/presets.jl")
-
-latex_name(name::Symbol) = latex_name(Val(name))
-latex_name(name::String) = latex_name(Symbol(name))
-latex_name(::Val{:num_init_bphs}) = "N_I"
-latex_name(::Val{:num_bphs}) = "n_A"
-latex_name(::Val{:num_nymphs}) = "n_N"
-latex_name(::Val{:num_eggs}) = "n_E"
-latex_name(::Val{:num_brachys}) = "n_B"
-latex_name(::Val{:num_macros}) = "n_M"
-latex_name(::Val{:pct_rices}) = "r_R"
-latex_name(::Val{:pct_nymphs}) = "r_N"
-latex_name(::Val{:pct_brachys}) = "r_B"
-latex_name(::Val{:pct_macros}) = "r_M"
-latex_name(::Val{:energy_transfer}) = "E_T"
 
 end # Visualisations module
