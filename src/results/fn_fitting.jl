8496a596116e576110c933a756bde17039eda14b
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-04 20:13:50 +0700

    wip: fit by factor

--- src/results/fn_fitting.jl
+++ src/results/fn_fitting.jl
@@ -12,8 +12,29 @@ function _legitify(x, fallback)
     end
 end
 
+function try_fit(model, x, y, base_params, names)
+    fit = try
+        curve_fit(model, x, y, base_params)
+    catch e
+        if e isa LinearAlgebra.SingularException
+            (; param = base_params, converged = false)
+        else
+            rethrow(e)
+        end
+    end
+
+    # Collect result 
+    ret_p = NamedTuple(k => v for (k, v) in zip(names, fit.param))
+    ret_c = (; converged = fit.converged)
+    return merge(ret_p, ret_c)
+end
+
+function is_stable(x)
+    3 * std(x) <= mean(x) * 0.05f0
+end
+
 function compute_global_stats(params_df, names)
-    gstats = map([mean, std, minimum, maximum]) do func
+    gstats = map([mean, std, minimum, maximum, is_stable]) do func
         mapreduce(merge, names) do name
             Dict(:stat => Symbol(func), name => func(params_df[!, name]))
         end
@@ -37,28 +58,20 @@ function group_fit(model, results, column;
     pbar = Progress(length(groups))
 
     combine(groups) do g
-        steps = Colon()
-        x = g[steps, :step]
-        y = g[steps, column]
+        # Use data from which step
+        steps = if stablesteps
+            1:argmax(g.num_bphs)
+        else
+            Colon()
+        end
+        x = g[steps, :step] * 1.0f0
+        y = g[steps, column] * 1.0f0
 
         # Try to fit the model
         base_params = init_params(model, x, y)
-        fit = try
-            curve_fit(model, x, y, base_params)
-        catch e
-            if e isa LinearAlgebra.SingularException
-                (; param = base_params, converged = false)
-            else
-                rethrow(e)
-            end
-        end
-
-        # Collect result 
-        ret_p = NamedTuple(k => v for (k, v) in zip(names, fit.param))
-        ret_c = (; converged = fit.converged)
-
+        result = try_fit(model, x, y, base_params, names)
         next!(pbar)
-        return merge(ret_p, ret_c)
+        return result
     end
 end
 
@@ -72,20 +85,19 @@ value of the input factor.
 function factor_group_fit(model, results, column; stablesteps = false)
     factor = get_factor_name(results)
     names = get_param_names(model)
-    fgroups = groupby(results, factor)
+    groups = groupby(results, factor)
 
-    combine(fgroups) do fg
-        steps = if stablesteps
-            _steps = map(groupsby(fg, :seed)) do sg
-                argmax(sg[!, :num_bphs])
-            end
-            max_step = trunc(Int, mean(_steps))
-            1:max_step
-        else
-            Colon()
-        end
+    pbar = Progress(length(groups))
+    combine(groups) do g
+        steps = Colon()
 
-        x = results[steps, :step]
-        y = results[steps, column]
+        x = g[steps, :step]
+        y = g[steps, column]
+
+        # Fit parameters
+        base_params = init_params(model, x, y)
+        result = try_fit(model, x, y, base_params, names)
+        next!(pbar)
+        return result
     end
 end
