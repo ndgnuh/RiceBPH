126c4f6a68be2b09d1ed243500fcd69b25ca6f2a
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-17 15:39:01 +0700

    visualize: font stuffs

--- scripts/replicate-figures.jl
+++ scripts/replicate-figures.jl
@@ -1,39 +1,68 @@
-using RiceBPH.Results: Result, visualize_qcv, compute_observations, visualize_num_bphs
-using GLMakie
+using RiceBPH.Results:
+   Result,
+   visualize_qcv,
+   compute_observations,
+   visualize_num_bphs,
+   visualize_pct_nymphs
+using CairoMakie
+
+const px_per_unit = 3
 
 function main()
-    # Prepare
-    figdir = "figures"
-    mkpath(figdir)
-    update_theme!(; fonts = (; regular = "serif",
-                             bold = "serif bold"))
+   # Prepare
+   figdir = "figures"
+   mkpath(figdir)
+   update_theme!(;
+      fonts = (;
+         regular = "fonts/NewComputerModern/NewCM10-Regular.otf",
+         bold = "fonts/NewComputerModern/NewCM10-Bold.otf",
+      ),
+   )
 
-    # Fix num init bphs plots
-    result = Result("outputs/num-init-bphs/")
-    fig = visualize_num_bphs(result)
-    output_file = joinpath(figdir, "param-fix-num-init-bphs.png")
-    save(output_file, fig)
-    @info "Figure written to $output_file"
-    GC.gc()
+   # Fix num init bphs plots
+   result = Result("outputs/num-init-bphs/")
+   fig = visualize_num_bphs(result)
+   output_file = joinpath(
+      figdir, "param-fix-num-init-bphs.png"
+   )
+   save(output_file, fig; px_per_unit)
+   @info "Figure written to $output_file"
+   result = nothing
+   GC.gc()
 
-    #  result
-    results = ("outputs/energy-transfer/",
-               "outputs/num-init-bphs/",
-               "outputs/init-pr-eliminate/",
-               "outputs/flower-width/")
-    for result_folder in results
-        @info "Processing $result_folder"
-        result = Result(result_folder)
-        df = compute_observations(result)
-        GC.gc()
+   # Fix energy transfer plot
+   result = Result("outputs/energy-transfer/")
+   fig = visualize_pct_nymphs(result)
+   output_file = joinpath(
+      figdir, "param-fix-energy-transfer.png"
+   )
+   save(output_file, fig; px_per_unit)
+   @info "Figure written to $output_file"
+   result = nothing
+   GC.gc()
 
-        # Stability analysis plots
-        fig, _, _ = visualize_qcv(result, df)
-        output_file = joinpath(figdir, "stability-$(result.factor_name).png")
-        save(output_file, fig)
-        @info "Figure written to $output_file"
-        GC.gc()
-    end
+   #  result
+   results = (
+      "outputs/energy-transfer/",
+      "outputs/num-init-bphs/",
+      "outputs/init-pr-eliminate/",
+      "outputs/flower-width/",
+   )
+   for result_folder in results
+      @info "Processing $result_folder"
+      result = Result(result_folder)
+      df = compute_observations(result)
+      GC.gc()
+
+      # Stability analysis plots
+      fig = visualize_qcv(result, df)
+      output_file = joinpath(
+         figdir, "stability-$(result.factor_name).png"
+      )
+      save(output_file, fig; px_per_unit)
+      @info "Figure written to $output_file"
+      GC.gc()
+   end
 end
 
 main()
