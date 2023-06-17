bf0a15ad9e183bec158e0a53e4191f4236b07071
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-06-17 16:37:23 +0700

    experiment: add paramscan, missing seed, compress result

--- src/Experiments/config.jl
+++ src/Experiments/config.jl
@@ -1,30 +1,43 @@
 @option struct ModelExploration
-    params::Models.ModelParameters
-    seed::Maybe{Int}
+   params::Models.ModelParameters
+   seed::Maybe{Int}
 end
 
 @option struct ModelVideo
-    params::Models.ModelParameters
-    video_output::String
-    num_steps::Int = 2880
-    spu::Int = 1
-    framerate::Int = 60
-    seed::Maybe{Int} = nothing
+   params::Models.ModelParameters
+   video_output::String
+   num_steps::Int = 2880
+   spu::Int = 1
+   framerate::Int = 60
+   seed::Maybe{Int} = nothing
 end
 
 @option struct ModelOFAT
-    factor::String
-    values::String
-    output::String
-    num_replications::Int
-    num_steps::Int
-    params::Dict
+   factor::String
+   values::String
+   output::String
+   num_replications::Int
+   num_steps::Int
+   params::Dict
 end
 
-SupportedConfig = Union{ModelExploration,
-                        ModelVideo, Vector{ModelVideo},
-                        ModelOFAT, Vector{ModelOFAT},
-                        PlotMeanStdTimeStep, Vector{PlotMeanStdTimeStep}}
+@option struct ModelParamScan
+   output::String
+   num_replications::Int
+   num_steps::Int
+   params::Dict{String, Vector}
+end
+
+SupportedConfig = Union{
+   ModelExploration,
+   ModelVideo,
+   Vector{ModelVideo},
+   ModelOFAT,
+   Vector{ModelOFAT},
+   PlotMeanStdTimeStep,
+   Vector{PlotMeanStdTimeStep},
+   ModelParamScan,
+}
 @option struct RunConfig
-    config::SupportedConfig
+   config::SupportedConfig
 end
--- src/Experiments/run.jl
+++ src/Experiments/run.jl
@@ -1,89 +1,177 @@
 function run(config::ModelVideo)
-    #
-    # Guard to not overwrite the existing
-    #
-    output = config.video_output
-    if isfile(output) || isdir(output)
-        @warn "The output path $(output) exists, ignoring. Delete the output to rerun."
-        return
-    end
-    touch(output) # Create to prevent a parallel run
+   #
+   # Guard to not overwrite the existing
+   #
+   output = config.video_output
+   if isfile(output) || isdir(output)
+      @warn "The output path $(output) exists, ignoring. Delete the output to rerun."
+      return nothing
+   end
+   touch(output) # Create to prevent a parallel run
 
-    #
-    # Create video
-    #
-    model = init_model(config.params; config.seed)
-    frames = config.num_steps
-    abmvideo(config.video_output, model, agent_step!, model_step!;
-             config.framerate, config.spu, frames,
-             showstep = true,
-             Visualisations.ac,
-             Visualisations.heatkwargs,
-             Visualisations.heatarray)
-    @info "Video written to $(output)"
+   #
+   # Create video
+   #
+   model = init_model(config.params; config.seed)
+   frames = config.num_steps
+   abmvideo(
+      config.video_output,
+      model,
+      agent_step!,
+      model_step!;
+      config.framerate,
+      config.spu,
+      frames,
+      showstep = true,
+      Visualisations.ac,
+      Visualisations.heatkwargs,
+      Visualisations.heatarray,
+   )
+   @info "Video written to $(output)"
 end
 
 function run(config::ModelExploration)
-    model = init_model(config.params; config.seed)
-    fig, _ = abmexploration(model; agent_step!, model_step!,
-                            mdata = Models.MDATA_EXPL,
-                            Visualisations.ac,
-                            Visualisations.heatkwargs,
-                            Visualisations.heatarray)
-    scene = display(fig)
-    wait(scene)
+   model = init_model(config.params; config.seed)
+   fig, _ = abmexploration(
+      model;
+      agent_step!,
+      model_step!,
+      mdata = Models.MDATA_EXPL,
+      Visualisations.ac,
+      Visualisations.heatkwargs,
+      Visualisations.heatarray,
+   )
+   scene = display(fig)
+   wait(scene)
 end
 
 function run(config::ModelOFAT)
-    #
-    # Guard to not overwrite the existing
-    #
-    output = config.output
-    if isfile(output) || isdir(output)
-        @warn "The output path $(output) exists, ignoring. Delete the output directory to rerun."
-        return
-    end
-    mkpath(output) # Create to prevent a parallel run
+   #
+   # Guard to not overwrite the existing
+   #
+   output = config.output
+   if isfile(output) || isdir(output)
+      @warn "The output path $(output) exists, ignoring. Delete the output directory to rerun."
+      return nothing
+   end
+   mkpath(output) # Create to prevent a parallel run
 
-    #
-    # Digging information from the config
-    #
-    factor = Symbol(config.factor)
-    values = eval(Meta.parse(config.values))
-    num_replications = config.num_replications
-    num_steps = config.num_steps
+   #
+   # Digging information from the config
+   #
+   factor = Symbol(config.factor)
+   values = eval(Meta.parse(config.values))
+   num_replications = config.num_replications
+   num_steps = config.num_steps
 
-    #
-    # Run the OFAT
-    #
-    result = mapreduce(vcat, values) do value
-        @info "Running $(factor) = $(value)"
-        # Prepare parameters
-        params = Dict(Symbol(k) => v for (k, v) in config.params)
-        params[factor] = value
+   #
+   # Run the OFAT
+   #
+   result = mapreduce(vcat, values) do value
+      @info "Running $(factor) = $(value)"
+      # Prepare parameters
+      params = Dict(
+         Symbol(k) => v for (k, v) in config.params
+      )
+      params[factor] = value
 
-        # Run with each seed
-        results = @showprogress pmap(1:num_replications) do seed
-            # Init and run
-            model = init_model(; params...)
-            _, mdf = run!(model, agent_step!, model_step!, num_steps;
-                          mdata = Models.MDATA)
+      # Run with each seed
+      results = @showprogress pmap(
+         1:num_replications
+      ) do seed
+         # Init and run
+         model = init_model(; params..., seed = seed)
+         _, mdf = run!(
+            model,
+            agent_step!,
+            model_step!,
+            num_steps;
+            mdata = Models.MDATA,
+         )
 
-            # Populate with factor name and seed
-            num_rows = size(mdf, 1)
-            mdf[!, factor] = fill(value, num_rows)
-            mdf[!, :seed] = fill(seed, num_rows)
+         # Populate with factor name and seed
+         num_rows = size(mdf, 1)
+         mdf[!, factor] = fill(value, num_rows)
+         mdf[!, :seed] = fill(seed, num_rows)
 
-            # Without this, oom
-            GC.gc()
-            return mdf
-        end
-        agg = reduce(vcat, results)
-        return agg
-    end
+         # Without this, oom
+         type_compress!(mdf; compress_float = true)
+         GC.gc()
+         return mdf
+      end
+      agg = reduce(vcat, results)
+      return agg
+   end
 
-    #
-    # Store results
-    #
-    JDF.save(output, result)
+   #
+   # Store results
+   #
+   JDF.save(output, result)
+end
+
+function run(config::ModelParamScan)
+   # Other meta data
+   output = config.output
+   num_steps = config.num_steps
+   num_replications = config.num_replications
+
+   # Look for existing output
+   if isfile(output) || isdir(output)
+      @warn "The output path $(output) exists, ignoring. Delete the output to rerun."
+      return nothing
+   end
+   mkpath(output) # Create to prevent a parallel run
+
+   # Construct parameter set
+   configurations = @chain begin
+      (
+         Symbol(key) => values for
+         (key, values) in config.params
+      )
+      (
+         (key => value for value in values) for
+         (key, values) in _
+      )
+      Iterators.product(_...)
+   end
+
+   # Replicate
+   results = mapreduce(vcat, configurations) do params
+      @info "Running configuration: $(params)"
+
+      # Run with each seed
+      results = @showprogress pmap(
+         1:num_replications
+      ) do seed
+         # Init and run
+         model = init_model(; seed = seed, params...)
+         _, mdf = run!(
+            model,
+            agent_step!,
+            model_step!,
+            num_steps;
+            mdata = Models.MDATA,
+         )
+
+         # Populate with factor name and seed
+         num_rows = size(mdf, 1)
+         for (key, value) in params
+            mdf[!, key] = fill(value, num_rows)
+         end
+         mdf[!, :seed] = fill(seed, num_rows)
+
+         # Without this, oom
+         type_compress!(mdf; compress_float = true)
+         GC.gc()
+         return mdf
+      end
+      agg = reduce(vcat, results)
+      return agg
+   end
+
+   #
+   # Store results
+   #
+   JDF.save(output, results)
+   @info "Output written to $(output)"
 end
