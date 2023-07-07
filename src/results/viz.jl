b34e9b38e50a44d970ed35e9bbbde1781e131f1c
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-07 10:42:06 +0700

    [wip] fix numbphs visualization

--- src/results/viz.jl
+++ src/results/viz.jl
@@ -1,14 +1,73 @@
+"""
+   dropnan(df)
+
+Remove rows with NaN from dataframe, returns a subdataframe.
+"""
+function dropnan(df)
+   mask = map(eachrow(df)) do row
+      mapreduce(!isnan, &, row)
+   end
+   @view df[mask, :]
+end
+
 const latex_font_size = 24
 function format_float(x)
    format(format"%.04f", x)
 end
 
 function visualize_qcv(
-   result::Result,
-   obdf = compute_observations(result);
-   plot_options = NamedTuple(),
-   axis_options = NamedTuple(),
-)
+      result::SimulationResult,
+      obdf = compute_observations(result);
+      plot_options = NamedTuple(),
+      axis_options = NamedTuple(),
+   )
+   df = rename(permutedims(obdf.qcv), [:qcv])
+   df.names = propertynames(obdf.qcv)
+
+   # Sort by QCV
+   df = sort(df, :qcv; rev = false)
+   names = df.names
+   qcv = df.qcv
+   idx = eachindex(names)
+
+   # Index of factor
+   factor_idx = indexin(result.factors, names)
+
+   colors = [
+             parse(Color, COLORSCHEME.color4),
+             parse(Color, COLORSCHEME.color2),
+            ]
+   colormap = map(idx) do i
+      if i in factor_idx
+         return colors[1]
+      else
+         return colors[2]
+      end
+   end
+
+   attrs = (;
+            bar_labels = map(x -> format(format"%.04f", x), qcv),
+            axis = (;
+                    xticks = (idx, map(latex_name, names)),
+                    xticklabelsize = latex_font_size,
+                    #= yticks = (0:0.1:maximum(qcv)+0.1), =#
+                    ylabel = ("Quartile Coefficent of Variant"),
+                    axis_options...,
+                   ),
+            color = colormap,
+            plot_options...,
+           )
+   fig, ax, _ = barplot(idx, qcv; attrs...)
+   #= ylims!(ax, 0, maximum(qcv) + 0.1) =#
+   return fig
+end
+
+function visualize_qcv(
+      result::Result,
+      obdf = compute_observations(result);
+      plot_options = NamedTuple(),
+      axis_options = NamedTuple(),
+   )
    df = rename(permutedims(obdf.qcv), [:qcv])
    df.names = DataFrames.names(obdf.qcv)
 
@@ -20,29 +79,29 @@ function visualize_qcv(
 
    # Index of factor
    factor_idx = findfirst(
-      result.factor_name |> string |> isequal, names
-   )
+                          result.factor_name |> string |> isequal, names
+                         )
 
    colors = [
-      parse(Color, COLORSCHEME.color4),
-      parse(Color, COLORSCHEME.color2),
-   ]
+             parse(Color, COLORSCHEME.color4),
+             parse(Color, COLORSCHEME.color2),
+            ]
    colormap = [
-      i == factor_idx ? colors[1] : colors[2] for i in idx
-   ]
+               i == factor_idx ? colors[1] : colors[2] for i in idx
+              ]
 
    attrs = (;
-      bar_labels = map(x -> format(format"%.04f", x), qcv),
-      axis = (;
-         xticks = (idx, map(latex_name, names)),
-         xticklabelsize = latex_font_size,
-         yticks = (0:0.1:maximum(qcv)+0.1),
-         ylabel = ("Quartile Coefficent of Variant"),
-         axis_options...,
-      ),
-      color = colormap,
-      plot_options...,
-   )
+            bar_labels = map(x -> format(format"%.04f", x), qcv),
+            axis = (;
+                    xticks = (idx, map(latex_name, names)),
+                    xticklabelsize = latex_font_size,
+                    yticks = (0:0.1:maximum(qcv)+0.1),
+                    ylabel = ("Quartile Coefficent of Variant"),
+                    axis_options...,
+                   ),
+            color = colormap,
+            plot_options...,
+           )
    fig, ax, _ = barplot(idx, qcv; attrs...)
    ylims!(ax, 0, maximum(qcv) + 0.1)
    return fig
@@ -50,31 +109,38 @@ end
 
 function visualize_num_bphs(result)
    fig = Figure()
+   df = (result.df)
+   factor_name = only(result.factors)
 
    # Axis setup
    day_step = 10
-   xticks = 0:(day_step*24):maximum(result.df.step)
+   xticks = 0:(day_step*24):maximum(df.step)
    ax = Axis(
-      fig[1, 1];
-      xticks = (xticks, (string ∘ Int).(xticks / 24)),
-      xlabel = "Day",
-      ylabel = "Number of BPHs",
-   )
+             fig[1, 1];
+             xticks = (xticks, (string ∘ Int).(xticks / 24)),
+             xlabel = "Day",
+             ylabel = "Number of BPHs",
+            )
 
    # For formatting the plot
    count = 1
    min_y = 0
-   max_y = maximum(result.df.num_bphs)
+   max_y = maximum(df.num_bphs)
 
    # Collect the result first
-   for group in groupby(result.df, result.factor_name)
-      stats = combine(groupby(group, :step)) do row
-         (;
-            t = first(unique(row.step)),
-            μ = mean(row.num_bphs),
-            σ = std(row.num_bphs),
-         )
+   for group in groupby(df, factor_name)
+      allstats = combine(groupby(group, :step)) do row
+         x = row.num_bphs
+         μ = mean(x)
+         σ = if length(x) == 1
+            only(x)
+         else
+            std(x)
+         end
+         (; t = first(unique(row.step)), μ, σ)
       end
+      display(allstats)
+      stats = dropnan(allstats)
 
       # Collect
       t = stats.t
@@ -83,15 +149,15 @@ function visualize_num_bphs(result)
 
       # Adjust axis scale
       max_y = max(
-         max_y, trunc(Int, maximum(μ + σ) / 100) * 100
-      )
+                  max_y, trunc(Int, maximum(μ + σ) / 100) * 100
+                 )
       min_y = min(
-         min_y, trunc(Int, minimum(μ - σ) / 100) * 100
-      )
+                  min_y, trunc(Int, minimum(μ - σ) / 100) * 100
+                 )
 
       # Formatting
       color = COLORSCHEME2[count]
-      factor_value = group[!, result.factor_name] |> first
+      factor_value = group[!, factor_name] |> first
       label = LaTeXString("\$N_I = $(factor_value)\$")
 
       # Plot mean + std plot with a band and a middle line
@@ -105,37 +171,37 @@ function visualize_num_bphs(result)
    # Reticks y axis
    ax.yticks = min_y:150:max_y
    fig[1, 2] = Legend(
-      fig,
-      ax,
-      "Initial number of BPHs";
-      framevisible = false,
-      nbanks = 2,
-   )
+                      fig,
+                      ax,
+                      "Initial number of BPHs";
+                      framevisible = false,
+                      nbanks = 2,
+                     )
    return fig
 end
 
 function visualize_pct_nymphs(
-   result,
-   obdf = compute_observations(result; by_factor = true),
-)
+      result,
+      obdf = compute_observations(result; by_factor = true),
+   )
    meandf = obdf.mean
-   factor_column = "$(result.factor_name)_value"
+   factor_column = "$(only(result.factors))_value"
 
    xvalues = meandf[!, factor_column]
    x = eachindex(xvalues)
    μ = meandf.pct_nymphs
 
    config = (;
-      bar_labels = map(format_float, μ),
-      axis = (;
-         xlabel = LaTeXString(latex_name(:pct_nymphs)),
-         ylabel = latex_name(result.factor_name),
-         yticks = (x, format_float.(xvalues)),
-         xticks = 0:0.1:1.3,
-      ),
-      direction = :x,
-      color = COLORSCHEME.color2,
-   )
+             bar_labels = map(format_float, μ),
+             axis = (;
+                     xlabel = LaTeXString(latex_name(:pct_nymphs)),
+                     ylabel = latex_name(only(result.factors)),
+                     yticks = (x, format_float.(xvalues)),
+                     xticks = 0:0.1:1.3,
+                    ),
+             direction = :x,
+             color = COLORSCHEME.color2,
+            )
    fig, ax, _ = barplot(x, μ; config...)
    ax.xlabelsize = latex_font_size
    ax.ylabelsize = latex_font_size
@@ -148,17 +214,17 @@ function heatmap_df(df, x, y, z; options...)
    ys = unique(df[!, y])
    zmap = groupby(df, Cols(x, y))
    z = map(
-      i -> first(zmap[i][!, z]), Iterators.product(xs, ys)
-   )
+           i -> first(zmap[i][!, z]), Iterators.product(xs, ys)
+          )
 
    fig = Figure()
    ax = Axis(
-      fig[1, 1];
-      xticks = (eachindex(xs), format_float.(xs)),
-      yticks = (eachindex(ys), format_float.(ys)),
-      xlabel = String(x),
-      ylabel = String(y),
-   )
+             fig[1, 1];
+             xticks = (eachindex(xs), format_float.(xs)),
+             yticks = (eachindex(ys), format_float.(ys)),
+             xlabel = String(x),
+             ylabel = String(y),
+            )
 
    # The heatmap
    hmap = heatmap!(ax, z; options...)
