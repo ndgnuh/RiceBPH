4f5b18ccc6a9502fcab2adc6709e50fcc08820fe
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-09 21:02:47 +0700

    just reformat

--- src/results/fit_bphs.jl
+++ src/results/fit_bphs.jl
@@ -1,14 +1,25 @@
-function fit_bphs(result::SimulationResult, groupkey=result.seed_factors)
+function fit_bphs(
+   result::SimulationResult, groupkey = result.seed_factors
+)
    groups = groupby(result.df, groupkey)
    combine(groups) do group
       # Stable steps
       q1 = quantile(group.pct_rices, 0.25)
       q3 = quantile(group.pct_rices, 0.75)
-      group = filter(:pct_rices => (x -> q1 < x < q3), group)
+      group = filter(
+         :pct_rices => (x -> q1 < x < q3), group
+      )
 
       # Statistics
-      results = map([:pct_nymphs, :pct_macros, :pct_brachys]) do name
-         name => mean(group[!, name])
+      results = map([
+         :pct_nymphs, :pct_macros, :pct_brachys
+      ]) do name
+         @chain begin
+            group[!, name]
+            filter(!isnan, _)
+            filter(!ismissing, _)
+            name => mean(_)
+         end
       end
       NamedTuple(results)
    end
--- src/results/fit_rices.jl
+++ src/results/fit_rices.jl
@@ -2,44 +2,46 @@ using DataFrames
 using LsqFit
 
 struct RiceLogistic
-    A::Float32
-    T_max::Float32
+   A::Float32
+   T_max::Float32
 end
 
 function (f::RiceLogistic)(t, params)
-    B, T = params
-    A = f.A
-    t = t / f.T_max
-    @. A + (1 - A) / (1 + exp(B * (t - T)))
+   B, T = params
+   A = f.A
+   t = t / f.T_max
+   @. A + (1 - A) / (1 + exp(B * (t - T)))
 end
 
 function get_param_basenames(_::RiceLogistic)
-    return [:spd_rices, :T]
+   return [:spd_rices, :T]
 end
 
-function fit_rices(result::SimulationResult, groupkey=result.seed_factors)
-    df::DataFrame = result.df
-    combine(groupby(df, groupkey)) do g
-        g = sort(g, :step)
+function fit_rices(
+   result::SimulationResult, groupkey = result.seed_factors
+)
+   df::DataFrame = result.df
+   combine(groupby(df, groupkey)) do g
+      g = sort(g, :step)
 
-        # Last rice
-        pct_rices = g.pct_rices[end]
-        T_max = g.step[end]
+      # Last rice
+      pct_rices = g.pct_rices[end]
+      T_max = g.step[end]
 
-        # Prepare function
-        f = RiceLogistic(pct_rices, T_max)
-        params = [1f0, 0.5f0]
+      # Prepare function
+      f = RiceLogistic(pct_rices, T_max)
+      params = [1.0f0, 0.5f0]
 
-        # Fitting
-        x = g.step * 1f0
-        y = g.pct_rices * 1f0
-        fit = try
-            curve_fit(f, x, y, params)
-        catch
-            (param = [NaN32, NaN32], converged=false)
-        end
+      # Fitting
+      x = g.step * 1.0f0
+      y = g.pct_rices * 1.0f0
+      fit = try
+         curve_fit(f, x, y, params)
+      catch
+         (param = [NaN32, NaN32], converged = false)
+      end
 
-        spd_rices = fit.converged ? first(fit.param) : NaN32
-        return (; pct_rices, spd_rices)
-    end
+      spd_rices = fit.converged ? first(fit.param) : NaN32
+      return (; pct_rices, spd_rices)
+   end
 end
