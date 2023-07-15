b3de67bceded455ba1ddd39316807330f4ddbc9d
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-15 09:33:07 +0700

    info on multi processing

--- scripts/main.jl
+++ scripts/main.jl
@@ -1,3 +1,8 @@
+using Distributed
+
+if nprocs() > 1
+   @info "Multi-processing detected, it might takes a while for all the processes to load the package"
+end
 using RiceBPH.Experiments
 
 Experiments.julia_main()
