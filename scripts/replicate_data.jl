9cdac247cbcf04044ba4af68c6d62259ef17da0f
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-07-17 20:18:50 +0700

    add presets to data replication scripts

--- scripts/replicate_data.jl
+++ scripts/replicate_data.jl
@@ -3,6 +3,13 @@ using Distributed
 import RiceBPH.Experiments as E
 
 function main()
+   # Dummy experiment to test the code
+   #= rm(E.SCAN_DUMMY.output, force=true, recursive=true) =#
+   #= rm(E.SOBOL_DUMMY.output, force=true, recursive=true) =#
+   #= run(E.SCAN_DUMMY) =#
+   #= run(E.SOBOL_DUMMY) =#
+
+   # Real experiment
    E.run(E.SOBOL_ET)
    E.run(E.SOBOL_ET_WIDE)
    E.run(E.SOBOL_N0)
@@ -11,5 +18,6 @@ function main()
    E.run(E.SCAN_N0)
    E.run(E.SCAN_ET)
    E.run(E.SCAN_SF_P0)
+   E.run(E.SCAN_SF_P0_WIDE)
 end
 main()
